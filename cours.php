eee3762d489a07b23612bacdefc08dde98ba4cbe
Benjamin Sonntag <benjamin@sonntag.fr>
2015-06-25 12:08:43 +0200

    no submodule for mediaelement

--- cours.php
+++ cours.php
@@ -13,8 +13,8 @@ $lesson=$cours[$id];
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
    <title>Cours de Crypto #<?php echo $id." - ".$lesson; ?></title>
 <link rel="stylesheet" href="/css/bootstrap.min.css">
-<link rel="stylesheet" href="/mediaelement/mediaelementplayer.css" />
-<script src="/mediaelement/build/mediaelement.js"></script>
+<link rel="stylesheet" href="/mediaelement/mediaelementplayer.min.css" />
+<script src="/mediaelement/mediaelement.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 </head>
 
