b5e52e0fd98a545f88abc4157a5bbd64d24aa153
Nadim Kobeissi <nadim@nadim.computer>
2015-06-25 15:24:38 +0200

    License

--- index.php
+++ index.php
@@ -37,6 +37,14 @@
 			?>
 		</ul>
 		
+		<p>
+			<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+				<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" />
+			</a>
+			<br />
+			<span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Cours de Cryptographie</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://nadim.computer" property="cc:attributionName" rel="cc:attributionURL">Nadim Kobeissi</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/kaepora/courscrypto" rel="dct:source">https://github.com/kaepora/courscrypto</a>.
+		</p>
+
 	</div>
 
 	<script src="/js/bootstrap.min.js"></script>
