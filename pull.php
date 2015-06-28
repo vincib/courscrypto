9729d2894ef637284eedd483490bf388506e0d18
Benjamin Sonntag <benjamin@octopuce.fr>
2015-06-28 10:03:28 +0200

    fixing pull?

--- pull.php
+++ pull.php
@@ -1,6 +1,13 @@
 <?php
 
-if (empty($_POST)) exit();
+$f=fopen("/var/alternc/html/c/courscrypto/log.txt","ab");
+fputs($f,date("Y-M-d H:m:s")." pull called\n");
+//if (empty($_POST)) exit();
+fputs($f,date("Y-M-d H:m:s")." POST non empty\n");
 chdir("/var/alternc/html/c/courscrypto/www");
-passthru("git pull 2>&1");
-
+exec("git pull 2>&1",$out,$ret);
+foreach($out as $line) {
+  fputs($f,date("Y-M-d H:m:s")." $line\n");
+}
+fputs($f,date("Y-M-d H:m:s")." exit code $ret\n");
+fclose($f);
