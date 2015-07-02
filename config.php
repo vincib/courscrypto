a377abd8dc1478167d8e244ceddb2d49832328f5
Benjamin Sonntag <benjamin@octopuce.fr>
2015-07-02 17:56:12 +0200

    adding second video session

--- config.php
+++ config.php
@@ -2,13 +2,16 @@
 
 $cours=array(
 	     1 => "Histoire de la cryptographie",
+	     3 => "Chiffrement de flux",
 	     );
 
 $slides=array(
 	      1 => "https://github.com/kaepora/courscrypto/raw/master/slides/Session1.pdf",
+	      2 => "https://github.com/kaepora/courscrypto/raw/master/slides/Session2.pdf",
 	      );
 
 $discussion=array(
 		  1 => "https://github.com/kaepora/courscrypto/issues/1",
+		  2 => "https://github.com/kaepora/courscrypto/issues/3",
 		  );
 
