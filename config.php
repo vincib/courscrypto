5f81f2666bbbbb99f7a7e06a73f6f2d8f5c41cea
Benjamin Sonntag <benjamin@octopuce.fr>
2015-07-06 16:50:37 +0200

    adding Session 3 video

--- config.php
+++ config.php
@@ -3,15 +3,18 @@
 $cours=array(
 	     1 => "Histoire de la cryptographie",
 	     2 => "Chiffrement de flux",
+	     3 => "Chiffrement par Bloc",
 	     );
 
 $slides=array(
 	      1 => "https://github.com/kaepora/courscrypto/raw/master/slides/Session1.pdf",
 	      2 => "https://github.com/kaepora/courscrypto/raw/master/slides/Session2.pdf",
+	      3 => "https://github.com/kaepora/courscrypto/raw/master/slides/Session3.pdf",
 	      );
 
 $discussion=array(
 		  1 => "https://github.com/kaepora/courscrypto/issues/1",
 		  2 => "https://github.com/kaepora/courscrypto/issues/3",
+		  3 => "https://github.com/kaepora/courscrypto/issues/6",
 		  );
 
